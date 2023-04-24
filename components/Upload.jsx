8c7f01a13be7aea888da9259517fe81cf4b627ee
Dinesh Rathod <pr760677@gmail.com>
2023-04-24 23:37:09 +0530

    Navbar removed from Upload

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -52,58 +52,58 @@ function UploadPodcast() {
 
 
     return (
-        <Layout>
-            <div className="max-w-md mx-auto my-8">
-                <h1 className="text-2xl font-semibold mb-4">Upload Your Podcast</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="podcast">
-                            Podcast
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="podcast"
-                            type="file"
-                            onChange={handleFileChange}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="title">
-                            Title
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="title"
-                            type="text"
-                            placeholder="Title"
-                            value={title}
-                            onChange={handleTitleChange}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
-                            Description
-                        </label>
-                        <textarea
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="description"
-                            type="text"
-                            placeholder="Description"
-                            value={description}
-                            onChange={handleDescriptionChange}
-                        />
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit"
-                        >
-                            Upload
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </Layout>
+
+        <div className="max-w-md mx-auto my-8">
+            <h1 className="text-2xl font-semibold mb-4">Upload Your Podcast</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="podcast">
+                        Podcast
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="podcast"
+                        type="file"
+                        onChange={handleFileChange}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="title">
+                        Title
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="title"
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
+                        Description
+                    </label>
+                    <textarea
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="description"
+                        type="text"
+                        placeholder="Description"
+                        value={description}
+                        onChange={handleDescriptionChange}
+                    />
+                </div>
+                <div className="flex items-center justify-center">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Upload
+                    </button>
+                </div>
+            </form>
+        </div>
+
     );
 }
 
